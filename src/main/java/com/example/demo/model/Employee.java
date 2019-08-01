7152aae1593975c2a35fef3c9b1be3d95c31c8d0
HS <francesco.johs@gmail.com>
2019-08-01 17:14:18 +0900

    init/yet DTable

--- src/main/java/com/example/demo/model/Employee.java
+++ src/main/java/com/example/demo/model/Employee.java
@@ -1,5 +1,4 @@
 package com.example.demo.model;
-
 import javax.persistence.*;
 
 @Entity
@@ -7,70 +6,230 @@ public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name = "id")
     private long id;
 
-    @Column(name="name")
-    private String name;
+    @Column(name = "SSN")
+    private long SSN;
 
-    @Column(name="last_name")
-    private String lastName;
+    @Column(name = "FNAME")
+    private String FNAME;
 
-    @Column(name="email")
-    private String email;
+    @Column(name = "LNAME")
+    private String LNAME;
 
-    @Column(name="phone")
-    private String phone;
+    @Column(name = "BDATE")
+    private String BDATE;
 
-    public Employee(){
+    @Column(name = "ADDRESS")
+    private String ADDRESS;
+
+    @Column(name = "SEX")
+    private String SEX;
+
+    @Column(name = "SALARY")
+    private long SALARY;
+
+    @Column(name = "SUPERSSN")
+    private long SUPERSSN;
+
+    @Column(name = "DNO")
+    private long DNO;
+
+    @Column(name = "PNUMBER")
+    private long PNUMBER;
+
+    @Column(name = "HOURS")
+    private long HOURS;
+
+    @Column(name = "DNUMBER")
+    private long DNUMBER;
+
+    @Column(name = "DNAME")
+    private String DNAME;
+
+    @Column(name = "MGRSTARTDATE")
+    private String MGRSTARTDATE;
+
+    @Column(name = "DLOCATION")
+    private String DLOCATION;
+
+    @Column(name = "PNAME")
+    private String PNAME;
+
+    @Column(name = "PLOCATION")
+    private String PLOCATION;
+
+    public Employee() {
         super();
     }
 
-    public Employee(String name, String lastName, String email, String phone){
+
+    public Employee(long SSN, String FNAME, String LNAME, String BDATE, String ADDRESS, String SEX, long SALARY, long SUPERSSN, long DNO, long PNUMBER, long HOURS, long DNUMBER, String DNAME, String MGRSTARTDATE, String DLOCATION, String PNAME, String PLOCATION) {
         super();
-        this.name=name;
-        this.lastName=lastName;
-        this.email=email;
-        this.phone=phone;
+        this.SSN = SSN;
+        this.FNAME = FNAME;
+        this.LNAME = LNAME;
+        this.BDATE = BDATE;
+        this.ADDRESS = ADDRESS;
+        this.SEX = SEX;
+        this.SALARY = SALARY;
+        this.SUPERSSN = SUPERSSN;
+        this.DNO = DNO;
+        this.PNUMBER = PNUMBER;
+        this.HOURS = HOURS;
+        this.DNUMBER = DNUMBER;
+        this.DNAME = DNAME;
+        this.MGRSTARTDATE = MGRSTARTDATE;
+        this.DLOCATION = DLOCATION;
+        this.PNAME = PNAME;
+        this.PLOCATION = PLOCATION;
     }
 
-    public long getId(){
+    public long getId() {
         return id;
     }
 
-    public void setId(long id){
+    public void setId(long id) {
         this.id = id;
     }
 
-    public String getName(){
-        return name;
+    public long getSSN() {
+        return SSN;
     }
 
-    public void setName(String name){
-        this.name=name;
+    public void setSSN(long SSN) {
+        this.SSN = SSN;
     }
 
-    public String getLastName(){
-        return lastName;
+    public String getFNAME() {
+        return FNAME;
     }
 
-    public void setLastName(String lastName){
-        this.lastName=lastName;
+    public void setFNAME(String FNAME) {
+        this.FNAME = FNAME;
     }
 
-    public String getEmail(){
-        return email;
+    public String getLNAME() {
+        return LNAME;
     }
 
-    public void setEmail(String email){
-        this.email = email;
+    public void setLNAME(String LNAME) {
+        this.LNAME = LNAME;
     }
 
-    public String getPhone(){
-        return phone;
+    public String getBDATE() {
+        return BDATE;
     }
 
-    public void setPhone(String phone){
-        this.phone = phone;
+    public void setBDATE(String BDATE) {
+        this.BDATE = BDATE;
+    }
+
+    public String getADDRESS() {
+        return ADDRESS;
+    }
+
+    public void setADDRESS(String ADDRESS) {
+        this.ADDRESS = ADDRESS;
+    }
+
+    public String getSEX() {
+        return SEX;
+    }
+
+    public void setSEX(String SEX) {
+        this.SEX = SEX;
+    }
+
+    public long getSALARY() {
+        return SALARY;
+    }
+
+    public void setSALARY(long SALARY) {
+        this.SALARY = SALARY;
+    }
+
+    public long getSUPERSSN() {
+        return SUPERSSN;
+    }
+
+    public void setSUPERSSN(long SUPERSSN) {
+        this.SUPERSSN = SUPERSSN;
+    }
+
+    public long getDNO() {
+        return DNO;
+    }
+
+    public void setDNO(long DNO) {
+        this.DNO = DNO;
+    }
+
+    public long getPNUMBER() {
+        return PNUMBER;
+    }
+
+    public void setPNUMBER(long PNUMBER) {
+        this.PNUMBER = PNUMBER;
+    }
+
+    public long getHOURS() {
+        return HOURS;
+    }
+
+    public void setHOURS(long HOURS) {
+        this.HOURS = HOURS;
+    }
+
+    public long getDNUMBER() {
+        return DNUMBER;
+    }
+
+    public void setDNUMBER(long DNUMBER) {
+        this.DNUMBER = DNUMBER;
+    }
+
+    public String getDNAME() {
+        return DNAME;
+    }
+
+    public void setDNAME(String DNAME) {
+        this.DNAME = DNAME;
+    }
+
+    public String getMGRSTARTDATE() {
+        return MGRSTARTDATE;
+    }
+
+    public void setMGRSTARTDATE(String MGRSTARTDATE) {
+        this.MGRSTARTDATE = MGRSTARTDATE;
+    }
+
+    public String getDLOCATION() {
+        return DLOCATION;
+    }
+
+    public void setDLOCATION(String DLOCATION) {
+        this.DLOCATION = DLOCATION;
+    }
+
+    public String getPNAME() {
+        return PNAME;
+    }
+
+    public void setPNAME(String PNAME) {
+        this.PNAME = PNAME;
+    }
+
+    public String getPLOCATION() {
+        return PLOCATION;
+    }
+
+    public void setPLOCATION(String PLOCATION) {
+        this.PLOCATION = PLOCATION;
     }
 }
+
+
+
--- src/main/resources/static/js/dataTable.js
+++ src/main/resources/static/js/dataTable.js
@@ -1,14 +1,58 @@
-$(document).ready( function () {
-	 var table = $('#employeesTable').DataTable({
-			"sAjaxSource": "/employees",
-			"sAjaxDataProp": "",
-			"order": [[ 0, "asc" ]],
-			"aoColumns": [
-			      { "mData": "id"},
-		          { "mData": "name" },
-				  { "mData": "lastName" },
-				  { "mData": "email" },
-				  { "mData": "phone" }
-			]
-	 })
-});
+
+//$(document).ready( function () {
+//	 var table = $('#employeesTable').DataTable({
+//			"sAjaxSource": "/employees",
+//			"sAjaxDataProp": "",
+//			"order": [[ 0, "asc" ]],
+//			"data": "Employee",
+//			"aoColumns": [
+//			      { "mData": "id"},
+//		          { "mData": "SSN" },
+//				  { "mData": "FNAME" },
+//				  { "mData": "LNAME" },
+//				  { "mData": "BDATE" },
+//				  { "mData": "ADDRESS" },
+//				  { "mData": "SEX" },
+//				  { "mData": "SALARY" },
+//				  { "mData": "SUPERSSN" },
+//				  { "mData": "DNO" },
+//				  { "mData": "PNUMBER" },
+//				  { "mData": "HOURS" },
+//				  { "mData": "DNUMBER" },
+//				  { "mData": "DNAME"},
+//				  { "mData": "MGRSTARTDATE" },
+//				  { "mData": "DLOCATION" },
+//				  { "mData": "PNAME" },
+//				  { "mData": "PLOCATION"}
+//
+//			]
+//	 })
+//});
+// jQuery dataTable
+// Ajax data
+$(document).ready(function () {
+                            $('#employeesTable').DataTable( {
+                             "ajax": {
+                             url:"/employees"},
+                                "columns": [
+                                    { "data": "id" },
+                                    { "mData": "SSN" },
+                                    { "mData": "FNAME" },
+                                    { "mData": "LNAME" },
+                                    { "mData": "BDATE" },
+                                    { "mData": "ADDRESS" },
+                                    { "mData": "SEX" },
+                                    { "mData": "SALARY" },
+                                    { "mData": "SUPERSSN" },
+                                    { "mData": "DNO" },
+                                    { "mData": "PNUMBER" },
+                                    { "mData": "HOURS" },
+                                    { "mData": "DNUMBER" },
+                                    { "mData": "DNAME"},
+                                    { "mData": "MGRSTARTDATE" },
+                                    { "mData": "DLOCATION" },
+                                    { "mData": "PNAME" },
+                                    { "mData": "PLOCATION"}
+                                ]
+                            } );
+                });
