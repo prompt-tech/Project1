a6f19a40c5c33496dabac9df6febe4b65e6b6929
billiamchoi <billiamchoi4u@gmail.com>
2019-08-04 20:28:38 +0900

    Column Filter add

--- src/main/resources/static/js/dataTable.js
+++ src/main/resources/static/js/dataTable.js
@@ -1,7 +1,23 @@
 // jQuery dataTable
 // Ajax data
 $(document).ready( function () {
+     $('#employeesTable thead tr').clone(true).appendTo( '#employeesTable thead' );
+     $('#employeesTable thead tr:eq(1) th').each( function (i) {
+         var title = $(this).text();
+         $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
+
+         $( 'input', this ).on( 'keyup change', function () {
+             if ( table.column(i).search() !== this.value ) {
+                 table
+                     .column(i)
+                     .search( this.value )
+                     .draw();
+             }
+         } );
+     } );
 	 var table = $('#employeesTable').DataTable({
+	    orderCellsTop: true,
+	    fixedHeader: true,
 			"sAjaxSource": "/employees",
 			"sAjaxDataProp": "",
 			"order": [[ 0, "asc" ]],
